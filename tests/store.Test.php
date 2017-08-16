4d332a6f45bb2d612cc481a2da40bfa945ecc6d5
Auke van Slooten <auke@poefke.nl>
2017-08-16 10:50:15 +0200

    added tests for delete changed to generator for store->ls/find

--- tests/store.Test.php
+++ tests/store.Test.php
@@ -12,64 +12,72 @@
 
     class TestStore extends PHPUnit_Framework_TestCase
     {
-    	var $db = null;
-    	var $store = null;
+        var $store = null;
 
-		function __construct()
-		{
-			parent::__construct();
-			$this->db = new PDO('pgsql:host=localhost;dbname=arc_store_test;user=auke;password=fropfrop');
-			$this->store = \arc\store::connect($this->db);
-			$this->store->initialize();
-		}
+        function __construct()
+        {
+            parent::__construct();
+            $this->store = \arc\store::connect('pgsql:host=localhost;dbname=arc_store_test;user=arc_store_test;password=test');
+            $this->store->initialize();
+        }
 
-		function testStoreQuery()
-		{
-			$qp = new \arc\store\PSQLQueryParser();
-			$result = $qp->parse("nodes.path='/'");
-			$this->assertEquals("select * from nodes where nodes.path='/'", $result);
-			$result = $qp->parse("foo.bar='baz'");
-			$this->assertEquals("select * from nodes where nodes.data #>> '{foo,bar}'='baz'", $result);
-		}
+        function testStoreQuery()
+        {
+            $qp = new \arc\store\PSQLQueryParser();
+            $result = $qp->parse("nodes.path='/'");
+            $this->assertEquals("select * from nodes where nodes.path='/'", $result);
+            $result = $qp->parse("foo.bar='baz'");
+            $this->assertEquals("select * from nodes where nodes.data #>> '{foo,bar}'='baz'", $result);
+        }
 
 
-		function testStoreSave()
-		{
-			$result = $this->store->save(\arc\lambda::prototype([
-				'name' => 'Foo',
-				'foo' => [
-					'bar' => 'Baz',
-					'int' => 10
-				]
-			]), '/foo/');
-			$this->assertTrue($result);
-		}
+        function testStoreSave()
+        {
+            $result = $this->store->save(\arc\lambda::prototype([
+                'name' => 'Foo',
+                'foo' => [
+                    'bar' => 'Baz',
+                    'int' => 10
+                ]
+            ]), '/foo/');
+            $this->assertTrue($result);
+        }
 
-		function testStoreExists()
-		{
-			$result = $this->store->exists('/foo/');
-			$this->assertTrue($result);
-		}
+        function testStoreExists()
+        {
+            $result = $this->store->exists('/foo/');
+            $this->assertTrue($result);
+        }
 
-		function testStoreLs()
-		{
-			$result = $this->store->ls('/');
-			$this->assertContainsOnly('stdClass',$result);
-			$this->assertCount(1, $result);
-		}
+        function testStoreLs()
+        {
+            $result = iterator_to_array($this->store->ls('/'));
+            $this->assertContainsOnly('stdClass',$result);
+            $this->assertCount(1, $result);
+        }
 
-		function testStoreFind()
-		{
-			$result = $this->store->find("nodes.path~='/%'");
-			$this->assertContainsOnly('stdClass',$result);
-			$this->assertCount(2, $result);
-			$result = $this->store->find("foo.bar~='Ba%'");
-			$this->assertCount(1, $result);
-		}
+        function testStoreFind()
+        {
+            $result = iterator_to_array($this->store->find("nodes.path~='/%'"));
+            $this->assertContainsOnly('stdClass',$result);
+            $this->assertCount(2, $result);
+            $result = iterator_to_array($this->store->find("foo.bar~='Ba%'"));
+            $this->assertCount(1, $result);
+        }
 
-		function testEnd()
-		{
-			$this->db->exec('drop table objects cascade');
-		}
-	
+        function testStoreDelete()
+        {
+            $result = $this->store->delete('/foo/');
+            $this->assertTrue($result);
+            $result = $this->store->exists('/foo/');
+            $this->assertFalse($result);
+        }
+
+        function testEnd()
+        {
+            $db = new PDO('pgsql:host=localhost;dbname=arc_store_test;user=arc_store_test;password=test');
+            $db->exec('drop table objects cascade');
+            $db->exec('drop table links cascade');
+        }
+    
     }
